c122855649344e1a0bfb821471dedcc21661a9df
JuveVR <42246813+JuveVR@users.noreply.github.com>
2019-02-18 18:03:44 +0200

    Adding new files

---
+++ Exercise_2.py
@@ -0,0 +1,18 @@
+# 2.2
+print(56 // 10)
+# 2.3
+c = str(156)
+summa = 0
+for i in c:
+    summa += int(i)
+print(summa)
+#second way
+d = 344
+d1 = d % 10
+d = d // 10
+d2 = d % 10
+d = d // 10
+d3 = d % 10
+
+sum = d1 + d2 + d3
+print(sum)
