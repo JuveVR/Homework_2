f6bb8631214840d30b44d5eb29e3d1c6ea21045c
JuveVR <42246813+JuveVR@users.noreply.github.com>
2019-02-18 18:03:58 +0200

    Add files via upload
    1

---
+++ Exercise_1.py
@@ -0,0 +1,16 @@
+# 1.1
+s = "some. random. string."
+print(s.isdigit())
+
+#1.2
+print(s.count(" "))
+
+#1.3
+print(s.count("."))
+
+#1.4
+s2 = "Homework"
+print(s2.center(100," "))
+
+#1.5
+print(s.title())
